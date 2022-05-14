e08bbe6c3052eb1ebe5a607a920ec50f11a76fad
Jose Espana <joseespana94@gmail.com>
2022-05-14 16:22:09 -0600

    seccion de aprendizaje terminada-propstypes,pruebas unitarias-

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,30 +1,26 @@
-//import React, { Fragment } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
+// import React, { Fragment } from 'react';
 
-
-const PrimeraApp = ( { 
-    saludo,
-    subtitulo
-
-} ) =>{
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
     return (
-< >
-    <h1> { saludo }</h1>
-    
-    {/*<h1> { JSON.stringify(saludo2) }  </h1>*/}
-    <p>{ subtitulo }</p>
-</>
+        <>
+            <h1>{ saludo }!!!</h1>
+            {/* <pre> {  JSON.stringify( saludo, null, 3 )  } </pre> */}
+            <p>{ subtitulo }</p>
+        </>
     );
+        
+} 
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
 }
 
-PrimeraApp.propTypes={
-    saludo:PropTypes.string.isRequired
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
-PrimeraApp.defaultProps={
-    subtitulo:'Soy un subtitulo'
-}
 
 export default PrimeraApp;
